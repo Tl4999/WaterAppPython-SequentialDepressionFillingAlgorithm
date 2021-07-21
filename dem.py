90077808203aeccf2b6b24533d86589ba4f1590b
Tl4999 <62628239+Tl4999@users.noreply.github.com>
2021-07-21 11:52:52 -0400

    Update dem.py

--- dem.py
+++ dem.py
@@ -1,25 +1,18 @@
-import rasterio
-import georasters as gr
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-#from rasterio.plot import show
-#demfile = r'CedarUpper_30m.tif'
-#img = rasterio.open(demfile)
-#show(img)
-
-file = gr.from_file('CedarUpper_30m.tif')
-print(gr.get_geo_info('CedarUpper_30m.tif'))
-NDV, xsize, ysize, GeoT, Projection,  DataType= gr.get_geo_info('CedarUpper_30m.tif')
-print('xsize',xsize, 'ysize', ysize)
-cellSize = GeoT[1]
-file = file.to_pandas()
-file = file.loc[:,['row','col','value']].to_numpy()
-print('file',file)
-dem = np.empty((ysize, xsize), dtype = float)
-
-for i in range(0,len(file)):
-    dem[int(file[i][0])][int(file[i][1])] = file[i][2]
-return dem,cellSize
+def elevationfile(fileName):
+    import rasterio
+    import georasters as gr
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    file = gr.from_file(fileName)
+    NDV, xsize, ysize, GeoT, Projection,  DataType= gr.get_geo_info(fileName)
+    cellSize = GeoT[1]
+    file = file.to_pandas()
+    file = file.loc[:,['row','col','value']].to_numpy()
+    dem = np.empty((ysize, xsize))
+    dem[:] = np.NaN
+    for i in range(0,np.shape(file)[0]):
+        dem[int(file[i][0])][int(file[i][1])] = file[i][2]
+    return dem,cellSize
 
