a55fe77f7d77566da59765dd0de8ee88fd792055
Tl4999 <62628239+Tl4999@users.noreply.github.com>
2021-07-22 12:02:22 -0400

    Update fillDepressionAlg.py

--- combo1/fillDepressionAlg.py
+++ combo1/fillDepressionAlg.py
@@ -1,7 +1,10 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 def fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellIndexes, pitID, pitCell, areaCellCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto, cellSize):
         import numpy as np
         #Prealllocate 
+        fillDem = np.copy(dem)
         max_id = max(pitID)
         rainfall_excess = np.zeros([max_id,1]) 
         times = np.zeros([max_id,1])
@@ -19,20 +22,21 @@ def fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellInd
         
         pitCell = np.array(pitCell) #Unravel_index return tupel. Turn tuple to array
                       
-        while vca[firstpit] <= (fillRainfallExcess/1000) and (idx <= max_id):
+        while vca[firstpit] <= ((fillRainfallExcess/1000) and (idx <= max_id)):
             #print('idx',idx,'firstpit',firstpit,'secondpit',secondpit)
             finalOrder[idx -1] = firstpit
             rainfall_excess[idx] = vca[firstpit][:]
             runoff[idx] = sum(sum(pits<0))
             
             #re_ID first pit, raise/ fill first pit cells
-            lessThanSpillover = [dem[cellIndexes[firstpit][:,0],cellIndexes[firstpit][:,1]] <= spilloverElevation[firstpit]] 
+            lessThanSpillover = [fillDem[cellIndexes[firstpit][:,0],cellIndexes[firstpit][:,1]] <= spilloverElevation[firstpit]] 
             lessThanSpillover = cellIndexes[firstpit][lessThanSpillover]
-            dem[lessThanSpillover[:,0],lessThanSpillover[:,1]] = spilloverElevation[firstpit] 
+            fillDem[lessThanSpillover[:,0],lessThanSpillover[:,1]] = spilloverElevation[firstpit] 
+            
             
             #Update Flow Direction
             flow_direction[pitCell[:,firstpit][0]][pitCell[:,firstpit][1]] = spilloverElevation[firstpit]
-            pits[cellIndexes[firstpit][:,0]][cellIndexes[firstpit][:,1]] = secondpit + 1
+            pits[cellIndexes[firstpit][:,0],cellIndexes[firstpit][:,1]] = secondpit + 1
             
             #HANDLE PITS CONNECTED TO THE EDGE OF DEM
             if secondpit > 0:
@@ -43,11 +47,10 @@ def fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellInd
                 keeper = pits[a[:,0],a[:,1]] != (secondpit + 1)
                 pairs[secondpit] = pairs[secondpit][keeper]
                 
-                #[val, ord] = min(max(dem(pairs{second_pit}(:, 1:2)), [], 2));
                 interior_rc = pairs[secondpit][:,0:2]
                 exterior_rc = pairs[secondpit][:,2:4]
-                interiorDEM = dem[interior_rc[:,0],interior_rc[:,1]]
-                exteriorDEM = dem[exterior_rc[:,0],exterior_rc[:,1]]
+                interiorDEM = fillDem[interior_rc[:,0],interior_rc[:,1]]
+                exteriorDEM = fillDem[exterior_rc[:,0],exterior_rc[:,1]]
                 cmax = np.empty(np.shape(interiorDEM))
                 for i in range(0,len(interiorDEM)):
                     cmax[i] = np.max([exteriorDEM[i],interiorDEM[i]])
@@ -62,10 +65,10 @@ def fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellInd
                 volume[secondpit] = filledVolume[secondpit];
                 cellIndexes[secondpit] = np.append(cellIndexes[secondpit],cellIndexes[firstpit],axis = 0)
                 
-                lessThans = [dem[cellIndexes[secondpit][:,0],cellIndexes[secondpit][:,1]] <= spilloverElevation[secondpit]];
+                lessThans = [fillDem[cellIndexes[secondpit][:,0],cellIndexes[secondpit][:,1]] <= spilloverElevation[secondpit]];
                 lessThans = cellIndexes[secondpit][lessThans]
                 
-                tempSpill = dem[lessThans[:,0],lessThans[:,1]]
+                tempSpill = fillDem[lessThans[:,0],lessThans[:,1]]
                 volume[secondpit] = volume[secondpit] + sum((spilloverElevation[secondpit] - tempSpill)*cellSize*cellSize);
                 lessThans = [];
 
@@ -79,13 +82,15 @@ def fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellInd
         
             idx += 1
         
-            firstpit = np.argmin(vca) #indexes of the first pit (scalar)
+            firstpit = np.nanargmin(vca) #indexes of the first pit (scalar)
             secondpit = int(pits[cellOverflowInto[firstpit][0]][cellOverflowInto[firstpit][1]] - 1)#indexes of the second pit (scalar)
+            
+        return dem, pits, flow_direction, rainfall_excess, runoff, fillDem
         
-fileName = 'CedarUpper_30m.tif'
+fileName = 'Feldun.tif'
 from dem import elevationfile
 dem,cellSize = elevationfile(fileName)
-dem = np.array(dem[230:241][:,540:551])
+#dem = np.array(dem[230:241][:,540:551])
 
 from flow_dir import flow_direction
 flow_direction = flow_direction(dem)
@@ -99,4 +104,11 @@ pits, pairs, cellIndexes, pitID, pitCell, areaCount, spilloverElevation, vca, vo
 
 fillRainfallExcess = 3000
 
-fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellIndexes, pitID, pitCell, areaCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto, cellSize)
+dem, fillPits, fillFlow_direction, fillRainfall_excess, fillRunoff, fillDem = fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellIndexes, pitID, pitCell, areaCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto, cellSize)
+
+fig, ax =plt.subplots(1,2)
+
+sns.heatmap(dem, ax=ax[0])
+sns.heatmap(fillDem, ax=ax[1])
+
+plt.show()
