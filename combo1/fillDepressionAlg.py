8e3d9eb0e837c853f4a06849eb703731e9af2aa1
Tl4999 <62628239+Tl4999@users.noreply.github.com>
2021-07-21 22:43:36 -0400

    Update fillDepressionAlg.py

--- combo1/fillDepressionAlg.py
+++ combo1/fillDepressionAlg.py
@@ -14,67 +14,89 @@ def fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellInd
         
         idx = 1
         
-        firstpit = np.argmin(vca) #indexes of the first pit
-        secondpits = pits[cellOverflowInto[firstpit][0]][cellOverflowInto[firstpit][1]]
+        firstpit = np.argmin(vca) #indexes of the first pit (scalar)
+        secondpit = int(pits[cellOverflowInto[firstpit][0]][cellOverflowInto[firstpit][1]] - 1)#indexes of the second pit (scalar)
         
-        #while vca[firstpit] <= (fillRainfallExcess/1000) and (idx <= max_id):
-            #finalOrder[idx -1] = firstpit
-            #rainfall_excess[idx] = vca[firstpit][:]
-            #runoff[idx] = sum(sum(pits<0))
+        pitCell = np.array(pitCell) #Unravel_index return tupel. Turn tuple to array
+                      
+        while vca[firstpit] <= (fillRainfallExcess/1000) and (idx <= max_id):
+            #print('idx',idx,'firstpit',firstpit,'secondpit',secondpit)
+            finalOrder[idx -1] = firstpit
+            rainfall_excess[idx] = vca[firstpit][:]
+            runoff[idx] = sum(sum(pits<0))
             
-            #lessThanSpillover = [dem[cellIndexes[firstpit][:,0],cellIndexes[firstpit][:,1]] <= spilloverElevation[firstpit]] 
-            #lessThanSpillover = cellIndexes[firstpit][lessThanSpillover]
-            #dem[lessThanSpillover[:,0],lessThanSpillover[:,1]] = secondpits
-            #flow_direction[pitCell[firstpit]]
+            #re_ID first pit, raise/ fill first pit cells
+            lessThanSpillover = [dem[cellIndexes[firstpit][:,0],cellIndexes[firstpit][:,1]] <= spilloverElevation[firstpit]] 
+            lessThanSpillover = cellIndexes[firstpit][lessThanSpillover]
+            dem[lessThanSpillover[:,0],lessThanSpillover[:,1]] = spilloverElevation[firstpit] 
             
+            #Update Flow Direction
+            flow_direction[pitCell[:,firstpit][0]][pitCell[:,firstpit][1]] = spilloverElevation[firstpit]
+            pits[cellIndexes[firstpit][:,0]][cellIndexes[firstpit][:,1]] = secondpit + 1
+            
+            #HANDLE PITS CONNECTED TO THE EDGE OF DEM
+            if secondpit > 0:
+                
+                areaCount[secondpit] = areaCount[firstpit] + areaCount[secondpit]
+                pairs[secondpit] = np.append(pairs[secondpit],pairs[firstpit],axis = 0)
+                a = pairs[secondpit][:,2:4]
+                keeper = pits[a[:,0],a[:,1]] != (secondpit + 1)
+                pairs[secondpit] = pairs[secondpit][keeper]
+                
+                #[val, ord] = min(max(dem(pairs{second_pit}(:, 1:2)), [], 2));
+                interior_rc = pairs[secondpit][:,0:2]
+                exterior_rc = pairs[secondpit][:,2:4]
+                interiorDEM = dem[interior_rc[:,0],interior_rc[:,1]]
+                exteriorDEM = dem[exterior_rc[:,0],exterior_rc[:,1]]
+                cmax = np.empty(np.shape(interiorDEM))
+                for i in range(0,len(interiorDEM)):
+                    cmax[i] = np.max([exteriorDEM[i],interiorDEM[i]])
+        
+                [val,cord] = [np.min(cmax), np.argmin(cmax)] 
+                
+                spilloverElevation[secondpit] = val
+                cellOverflowInto[secondpit] = exterior_rc[cord]
+                 
+                #compute filled volume and initialize volume calculation (more below)
+                filledVolume[secondpit] = volume[firstpit] + filledVolume[secondpit];
+                volume[secondpit] = filledVolume[secondpit];
+                cellIndexes[secondpit] = np.append(cellIndexes[secondpit],cellIndexes[firstpit],axis = 0)
+                
+                lessThans = [dem[cellIndexes[secondpit][:,0],cellIndexes[secondpit][:,1]] <= spilloverElevation[secondpit]];
+                lessThans = cellIndexes[secondpit][lessThans]
+                
+                tempSpill = dem[lessThans[:,0],lessThans[:,1]]
+                volume[secondpit] = volume[secondpit] + sum((spilloverElevation[secondpit] - tempSpill)*cellSize*cellSize);
+                lessThans = [];
+
+                vca[secondpit] = volume[secondpit]/((cellSize*cellSize)*areaCount[secondpit]);
+                if vca[secondpit] < 0:
+                    vca[secondpit] = np.infty;
+        
+            vca[firstpit] = np.NaN;     
+            cellIndexes[firstpit] = [];
+            pairs[firstpit] = [];
+        
+            idx += 1
+        
+            firstpit = np.argmin(vca) #indexes of the first pit (scalar)
+            secondpit = int(pits[cellOverflowInto[firstpit][0]][cellOverflowInto[firstpit][1]] - 1)#indexes of the second pit (scalar)
+        
+fileName = 'CedarUpper_30m.tif'
+from dem import elevationfile
+dem,cellSize = elevationfile(fileName)
+dem = np.array(dem[230:241][:,540:551])
+
+from flow_dir import flow_direction
+flow_direction = flow_direction(dem)
+test_flow_direction = flow_direction.astype(float)
+
+from d8FlowDirectionParents import d8FlowDirectionParents
+flow_direction_parent = d8FlowDirectionParents(dem)
+
 from getDepression import getDepression
-dem = np.array([[4, 7, 3, 7, 8, 8, 5, 2, 9, 8],
- [0, 8, 2, 4, 6, 4, 7, 3, 8, 5],
- [4, 5, 9, 9, 8, 3, 6, 4, 6, 6],
- [6, 1, 0, 5, 7, 7, 5, 8, 8, 7],
- [9, 9, 3, 5, 9, 7, 6, 9, 7, 2],
- [0, 2, 8, 0, 4, 0, 1, 2, 8, 7],
- [6, 9, 2, 9, 5, 5, 9, 1, 5, 9],
- [4, 3, 6, 9, 1, 3, 0, 8, 1, 5],
- [0, 1, 3, 7, 1, 9, 9, 4, 2, 8],
- [1, 4, 4, 6, 4, 4, 8, 9, 5, 9]])
+pits, pairs, cellIndexes, pitID, pitCell, areaCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto = getDepression(dem, flow_direction, flow_direction_parent,cellSize)
 
-flow_direction = np.array([[10., 10., 12.,  2., 13., 15.,  7., -2.,  7., 19.],
- [-2., 10., -1., 12., 25., 25., 17.,  7., 17., -2.],
- [10., 31., 32., 32., 25., -1., 25., 17., 17., 19.],
- [31., 32., -1., 32., 25., 25., 25., 27., 49., 49.],
- [50., 31., 32., 53., 53., 55., 56., 57., 49., -2.],
- [-2., 50., 53., -1., 53., -1., 55., 56., 57., 49.],
- [50., 51., 53., 53., 74., 55., 76., 76., 67., 78.],
- [80., 80., 62., 74., -1., 76., -1., 76., -1., 78.],
- [-2., 80., 81., 84., -1., 84., 76., 76., 78., 88.],
- [80., 81., 81., 84., 84., 84., 95., 87., 88., 88.]])
+fillRainfallExcess = 3000
 
-flow_direction_parent = np.array([[list([]), list([]), list([3]), list([]), list([]), list([]), list([]),
-  list([6, 8, 17]), list([]), list([])],
- [list([0, 1, 11, 20]), list([]), list([2, 13]), list([4]), list([]),
-  list([5]), list([]), list([16, 18, 27, 28]), list([]), list([9, 29])],
- [list([]), list([]), list([]), list([]), list([]),
-  list([14, 15, 24, 26, 34, 35, 36]), list([]), list([37]), list([]),
-  list([])],
- [list([]), list([21, 30, 41]), list([22, 23, 31, 33, 42]), list([]),
-  list([]), list([]), list([]), list([]), list([]), list([])],
- [list([]), list([]), list([]), list([]), list([]), list([]), list([]), list([]),
-  list([]), list([38, 39, 48, 59])],
- [list([40, 51, 60]), list([61]), list([]), list([43, 44, 52, 54, 62, 63]),
-  list([]), list([45, 56, 65]), list([46, 57]), list([47, 58]), list([]),
-  list([])],
- [list([]), list([]), list([72]), list([]), list([]), list([]), list([]),
-  list([68]), list([]), list([])],
- [list([]), list([]), list([]), list([]), list([64, 73]), list([]),
-  list([66, 67, 75, 77, 86, 87]), list([]), list([69, 79, 88]), list([])],
- [list([70, 71, 81, 90]), list([82, 91, 92]), list([]), list([]),
-  list([83, 85, 93, 94, 95]), list([]), list([]), list([97]),
-  list([89, 98, 99]), list([])],
- [list([]), list([]), list([]), list([]), list([]), list([96]), list([]),
-  list([]), list([]), list([])]], dtype = object)
-
-cellSize = 10
-pits, pairs, cellIndexes, pitID, pitCell, areaCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto = getDepression(dem, flow_direction, flow_direction_parent, cellSize)
-
-fillDepression(3000, dem, flow_direction, pits, pairs, cellIndexes, pitID, pitCell, areaCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto, cellSize)
+fillDepression(fillRainfallExcess, dem, flow_direction, pits, pairs, cellIndexes, pitID, pitCell, areaCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto, cellSize)
